3a916ad8bca8e60b3f2befaee641f8b2f95989ae
Justin Gordon <justin.gordon@gmail.com>
2014-02-15 21:36:02 -1000

    Upgrade to 2.99/3.0 rspec by using Rspec.current_example
    * @example was removed
    
    * passing example to the block was hiding access to method example
    
    * Use of example method was deprecated in favor of using
      Rspec.current_example

--- lib/rspec/retry.rb
+++ lib/rspec/retry.rb
@@ -10,24 +10,24 @@ module RSpec
         config.add_setting :default_retry_count, :default => 1
         config.add_setting :clear_lets_on_failure, :default => true
 
-        config.around(:each) do |example|
-          retry_count = example.metadata[:retry] || RSpec.configuration.default_retry_count
+        config.around(:each) do |ex|
+          retry_count = ex.metadata[:retry] || RSpec.configuration.default_retry_count
 
-          clear_lets = example.metadata[:clear_lets_on_failure]
+          clear_lets = ex.metadata[:clear_lets_on_failure]
           clear_lets = RSpec.configuration.clear_lets_on_failure if clear_lets.nil?
 
           retry_count.times do |i|
             if RSpec.configuration.verbose_retry?
               if i > 0
-                message = "RSpec::Retry: #{RSpec::Retry.ordinalize(i + 1)} try #{@example.location}"
+                message = "RSpec::Retry: #{RSpec::Retry.ordinalize(i + 1)} try #{RSpec.current_example.location}"
                 message = "\n" + message if i == 1
                 RSpec.configuration.reporter.message(message)
               end
             end
-            @example.clear_exception
-            example.run
+            RSpec.current_example.clear_exception
+            ex.run
 
-            break if @example.exception.nil?
+            break if RSpec.current_example.exception.nil?
 
             self.clear_lets if clear_lets
           end
