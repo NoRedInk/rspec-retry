c804b7bc9e90ab500518806697762723a09e28d4
Pavel.Lobashov <shockwavenn@gmail.com>
2015-06-23 12:09:50 +0300

    Rename `exceptions_to_ignore` to `exceptions_to_ignore_retry`

--- lib/rspec/retry.rb
+++ lib/rspec/retry.rb
@@ -16,7 +16,7 @@ module RSpec
         #
         # If no list of exceptions is provided and 'retry' > 1, we always retry.
         config.add_setting :exceptions_to_retry, :default => []
-        config.add_setting :exceptions_to_ignore, :default => []
+        config.add_setting :exceptions_to_ignore_retry, :default => []
 
         # context.example is deprecated, but RSpec.current_example is not
         # available until RSpec 3.0.
@@ -28,7 +28,7 @@ module RSpec
           retry_count = ex.metadata[:retry] || RSpec.configuration.default_retry_count
           sleep_interval = ex.metadata[:retry_wait] || RSpec.configuration.default_sleep_interval
           exceptions_to_retry = ex.metadata[:exceptions_to_retry] || RSpec.configuration.exceptions_to_retry
-          exceptions_to_ignore = ex.metadata[:exceptions_to_ignore] || RSpec.configuration.exceptions_to_ignore
+          exceptions_to_ignore_retry = ex.metadata[:exceptions_to_ignore_retry] || RSpec.configuration.exceptions_to_ignore_retry
 
           clear_lets = ex.metadata[:clear_lets_on_failure]
           clear_lets = RSpec.configuration.clear_lets_on_failure if clear_lets.nil?
@@ -50,8 +50,8 @@ module RSpec
               break unless exceptions_to_retry.include?(example.exception.class)
             end
 
-            if exceptions_to_ignore.any?
-              break if exceptions_to_ignore.include?(example.exception.class)
+            if exceptions_to_ignore_retry.any?
+              break if exceptions_to_ignore_retry.include?(example.exception.class)
             end
 
             self.clear_lets if clear_lets
