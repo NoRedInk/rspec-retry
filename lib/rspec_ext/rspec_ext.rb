2462866ef778762edc81726dd4abecf77e74f4fa
Eito Katagiri <eitoball@gmail.com>
2015-06-27 14:08:14 +0900

    clean let definitions

--- lib/rspec_ext/rspec_ext.rb
+++ lib/rspec_ext/rspec_ext.rb
@@ -11,10 +11,16 @@ end
 module RSpec
   module Core
     class ExampleGroup
+      def clear_memoized
+        # __memoized is private method and is defined in rspec 3.3
+        __memoized.instance_variable_get(:@memoized).clear
+      rescue NameError
+        @__memoized = nil
+      end
+
       def clear_lets
-        @__memoized = {}
+        clear_memoized
       end
     end
   end
 end
-
