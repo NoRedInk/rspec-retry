2c19048d4f0488e53c42026bb0d36e0608c38046
David Butler <dwbutler@ucla.edu>
2015-09-14 17:55:49 +0800

    Refactor so that retry can be called programmatically
    This allows RSpec Retry to be invoked programmatically in an around hook by calling `ex.run_with_retry(opts)`, which allows the behavior to be more customizable. Custom retry options can be passed into this method. Example:
    
    ```ruby
    config.around :each, :js do |ex|
      ex.run_with_retry retry: 3
    end
    ```
    
    Fixes #20, #44

--- lib/rspec/retry.rb
+++ lib/rspec/retry.rb
@@ -4,7 +4,7 @@ require 'rspec_ext/rspec_ext'
 
 module RSpec
   class Retry
-    def self.apply
+    def self.setup
       RSpec.configure do |config|
         config.add_setting :verbose_retry, :default => false
         config.add_setting :default_retry_count, :default => 1
@@ -18,59 +18,99 @@ module RSpec
         # If no list of exceptions is provided and 'retry' > 1, we always retry.
         config.add_setting :exceptions_to_retry, :default => []
 
-        # context.example is deprecated, but RSpec.current_example is not
-        # available until RSpec 3.0.
-        fetch_current_example = RSpec.respond_to?(:current_example) ?
-          proc { RSpec.current_example } : proc { |context| context.example }
-
         config.around(:each) do |ex|
-          example = fetch_current_example.call(self)
+          ex.run_with_retry
+        end
+      end
+    end
 
-          retry_count = [
-            (
-              ENV['RSPEC_RETRY_RETRY_COUNT'] ||
+    attr_reader :context, :ex
+
+    def initialize(ex, opts = {})
+      @ex = ex
+      @ex.metadata.merge!(opts)
+      current_example.attempts ||= 0
+    end
+
+    # context.example is deprecated, but RSpec.current_example is not
+    # available until RSpec 3.0.
+    def current_example
+      RSpec.respond_to?(:current_example) ?
+        RSpec.current_example : @ex.example
+    end
+
+    def retry_count
+      [
+          (
+          ENV['RSPEC_RETRY_RETRY_COUNT'] ||
               ex.metadata[:retry] ||
               RSpec.configuration.default_retry_count
-            ).to_i,
-            1
-          ].max
+          ).to_i,
+          1
+      ].max
+    end
 
-          sleep_interval = ex.metadata[:retry_wait] || RSpec.configuration.default_sleep_interval
-          exceptions_to_retry = ex.metadata[:exceptions_to_retry] || RSpec.configuration.exceptions_to_retry
+    def attempts
+      current_example.attempts ||= 0
+    end
 
-          clear_lets = ex.metadata[:clear_lets_on_failure]
-          clear_lets = RSpec.configuration.clear_lets_on_failure if clear_lets.nil?
+    def attempts=(val)
+      current_example.attempts = val
+    end
 
-          retry_count.times do |i|
-            if RSpec.configuration.verbose_retry?
-              if i > 0
-                message = "RSpec::Retry: #{RSpec::Retry.ordinalize(i + 1)} try #{example.location}"
-                message = "\n" + message if i == 1
-                RSpec.configuration.reporter.message(message)
-              end
-            end
-            example.clear_exception
-            ex.run
+    def clear_lets
+      !ex.metadata[:clear_lets_on_failure].nil? ?
+          ex.metadata[:clear_lets_on_failure] :
+          RSpec.configuration.clear_lets_on_failure
+    end
 
-            break if example.exception.nil?
+    def sleep_interval
+      ex.metadata[:retry_wait] ||
+          RSpec.configuration.default_sleep_interval
+    end
 
-            if exceptions_to_retry.any?
-              break unless exceptions_to_retry.any? do |exception_klass|
-                example.exception.is_a?(exception_klass)
-              end
-            end
+    def exceptions_to_retry
+      ex.metadata[:exceptions_to_retry] ||
+          RSpec.configuration.exceptions_to_retry
+    end
 
-            if RSpec.configuration.verbose_retry? && RSpec.configuration.display_try_failure_messages?
-              if i != (retry_count-1)
-                try_message = "#{RSpec::Retry.ordinalize(i + 1)} Try error in #{example.location}:\n #{example.exception.to_s} \n"
-                RSpec.configuration.reporter.message(try_message)
-              end
-            end
+    def run
+      example = current_example
 
-            self.clear_lets if clear_lets
-            sleep sleep_interval if sleep_interval.to_i > 0
+      loop do
+        if RSpec.configuration.verbose_retry?
+          if attempts > 0
+            message = "RSpec::Retry: #{RSpec::Retry.ordinalize(attempts + 1)} try #{example.location}"
+            message = "\n" + message if attempts == 1
+            RSpec.configuration.reporter.message(message)
           end
         end
+
+        example.clear_exception
+        ex.run
+
+        self.attempts += 1
+
+        break if example.exception.nil?
+
+        break if attempts >= retry_count
+
+        if exceptions_to_retry.any?
+          break unless exceptions_to_retry.any? do |exception_klass|
+            example.exception.is_a?(exception_klass)
+          end
+        end
+
+        if RSpec.configuration.verbose_retry? && RSpec.configuration.display_try_failure_messages?
+          if i != (retry_count-1)
+            try_message = "#{RSpec::Retry.ordinalize(i + 1)} Try error in #{example.location}:\n #{example.exception.to_s} \n"
+            RSpec.configuration.reporter.message(try_message)
+          end
+        end
+
+        example.example_group_instance.clear_lets if clear_lets
+
+        sleep sleep_interval if sleep_interval.to_i > 0
       end
     end
 
@@ -90,4 +130,4 @@ module RSpec
   end
 end
 
-RSpec::Retry.apply
+RSpec::Retry.setup
